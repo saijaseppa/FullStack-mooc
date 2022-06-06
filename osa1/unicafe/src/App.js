7ab58068f15c7b4daa7572ba937269e995231bf9
Saija <saija.seppa@tuni.fi>
2022-06-06 10:46:51 +0300

    1.6 - 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,65 @@
 import { useState } from "react";
 
+const Title = ({ text }) => <div><h1>{text}</h1></div>
+
 const Button = ({ handleClick, text }) => {
   return (
     <button onClick={handleClick}>{text}</button>
   )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) return <p>No feedback given</p>
+  else {
+    return (
+      <table>
+        <tbody>
+          <tr><StatisticLine text="good" value={good} /></tr>
+          <tr><StatisticLine text="neutral" value={neutral} /></tr>
+          <tr><StatisticLine text="bad" value={bad} /></tr>
+          <tr><StatisticLine text="all" value={good + neutral + bad} /></tr>
+          <tr><StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} /></tr>
+          <tr><StatisticLine text="positive" value={good / (good + neutral + bad) * 100} /></tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const StatisticLine = ({ text, value }) => {
+  if (text === "positive") return <td>{text} {value} %</td>
+  else {
+    return (
+      <td>{text} {value}</td>
+    )
+  }
+}
+
+
 const App = () => {
 
-  const [ good, setGood ] = useState(0);
-  const [ neutral, setNeutral ] = useState(0);
-  const [ bad, setBad ] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const addGood = () => {
-    setGood(good+1);
-  }
+  const addGood = () => setGood(good + 1);
+  const addNeutral = () => setNeutral(neutral + 1);
+  const addBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => addGood()} text="good"/>
+      <Title text="give feedback" />
+
+      <Button handleClick={() => addGood()} text="good" />
+      <Button handleClick={() => addNeutral()} text="neutral" />
+      <Button handleClick={() => addBad()} text="bad" />
+
+      <Title text="statistics" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+
+
     </div>
   )
 }
